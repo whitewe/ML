e5eea586b0a9058b1abad1b7cfcbd4593f3578c7
whitewe <31542938+whitewe@users.noreply.github.com>
2018-09-09 16:56:50 +0800

    dataSet

--- trees.py
+++ trees.py
@@ -8,6 +8,27 @@ def calcShannonEnt(dataSet):
         labelCounts[currentLabel] = labelCounts.get(currentLabel,0)+1
     shannonEnt = 0.0
     for key in labelCounts:
-        prob = labelCounts['key']/numEntries
+        prob = labelCounts[key]/numEntries
         shannonEnt -= prob*log(prob,2)
     return shannonEnt
+
+
+def createDataSet():
+    dataSet = [[1,1,'yes'],
+               [1,1,'yes'],
+               [1,0,'no'],
+               [0,1,'no'],
+               [0,1,'no']]
+    labels = ['no surfacing','flippers']
+    return dataSet,labels
+
+
+def main():
+    dataSet,labels = createDataSet()
+    dataSet[0][-1] = 'maybe'
+
+    print(calcShannonEnt(dataSet))
+
+
+if __name__=='__main__':
+    main()
